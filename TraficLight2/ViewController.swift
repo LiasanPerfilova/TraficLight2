0a8f76ca108e361ab993f2859e9677968d48ee6e
LiaisanPerfilova <perliaisan@gmail.com>
2023-05-18 16:58:50 -0400

    Initial Commit

---
+++ TraficLight2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraficLight2
+//
+//  Created by Liaisan on 5/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
